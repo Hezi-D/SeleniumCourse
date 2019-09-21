31f97540061aa160d40074f29e51161c012a5065
Hezi-D <dddavid1984@gmail.com>
2019-09-22 02:37:50 +0300

    second commit

---
+++ ConsoleApp2/002_Inerigation_Elements/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("002_Inerigation_Elements")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("002_Inerigation_Elements")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4450f4f0-b82c-42b5-b759-d5d7f196721b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ConsoleApp2/002_Inerigation_Elements/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+using System.Data;
+
+namespace _002_Inerigation_Elements
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            var driver = GetChromeDriver();
+            driver.Navigate().GoToUrl("https://ultimateqa.com/");
+
+            //GET WINDOW PROPERTIES
+            //Get window id:
+            var x = driver.CurrentWindowHandle;
+
+            //Get the open windows detail
+            //U can use id or index like: driver.WindowHandles[2]
+            var y = driver.WindowHandles;
+
+            //Get page source , lector never use it...
+            x = driver.PageSource;
+            x = driver.Title;
+            x = driver.Url;
+
+
+
+        }
+
+
+        private IWebDriver GetChromeDriver()
+        {
+            var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            return new ChromeDriver(outPutDirectory);
+        }
+    }
+
+
+
+}
